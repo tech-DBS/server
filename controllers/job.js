d713658789c0e6bdbc6032c02f988bb68e912db0
Rishav Singh <rishavrajcrj@gmail.com>
2025-02-02 03:33:56 +0530

    Update job.js

--- controllers/job.js
+++ controllers/job.js
@@ -4,6 +4,9 @@ const { validationResult } = require("express-validator");
 const fs = require("fs");
 const path = require("path");
 
+// csv
+const { parse } = require("json2csv");
+
 // Sheets
 const { google } = require("googleapis");
 
@@ -17,63 +20,91 @@ const auth = new google.auth.GoogleAuth({
 
 const genLetter = async (req, res, next) => {
   try {
-    const data = await readSheet();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    const PRFCO_API_KEY = process.env.PRFCO_API_KEY;
-
-    const SourceFileUrl =
-      "https://cloud.appwrite.io/v1/storage/buckets/679e8e45003cb356bbaa/files/679e8e7c003a8201ecee/view?project=679e8e0a0036c08d4ca7&mode=admin";
-
-    const Password = "";
-    const DestinationFile = "./result.pdf";
-
-    const apiUrl = "https://api.pdf.co/v1/pdf/edit/replace-text";
-
-    const requestData = {
-      name: path.basename(DestinationFile),
-      password: Password,
-      url: SourceFileUrl,
-      searchString: "Your Company Name",
-      replaceString: "XYZ LLC",
-    };
-
-    // Make API request to replace text in the PDF
-    const response = await axios.post(apiUrl, requestData, {
-      headers: {
-        "x-api-key": PRFCO_API_KEY,
-        "Content-Type": "application/json",
+    const { SourceFileUrl } = req.body,
+      Password = "",
+      data = await readSheet(),
+      apiUrl = process.env.API_URL,
+      DestinationFile = "./result.pdf",
+      PRFCO_API_KEY = process.env.PRFCO_API_KEY,
+      options = {
+        timeZone: "Asia/Kolkata",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
       },
-    });
+      currentDateIST = new Date().toLocaleDateString("en-IN", options);
 
-    if (response.data.error) {
-      console.log("Error from API:", response.data.message);
-      return res.status(500).json({ error: response.data.message });
-    }
-
-    if (response.data.url) {
-      return res.status(202).json({
-        status: true,
-        message: "Data Generated",
-        URL: response.data.url,
-      });
-    } else {
-      return res.status(202).json({
+    if (data.length === 0) {
+      return res.status(204).json({
         status: false,
-        message: "Unable to Generate",
-        URL: response.data.url,
+        message: "Unable to fetch data from sheet",
       });
     }
+
+    let pdfUrls = [];
+    for (let i = 1; i < data.length; i++) {
+      const [name, position] = data[i];
+
+      // const requestData = {
+      //   name: path.basename(DestinationFile),
+      //   password: Password,
+      //   url: SourceFileUrl,
+      //   searchStrings: ["{{name}}", "{{position}}", "{{date}}"],
+      //   replaceStrings: [name, position, currentDateIST],
+      // };
+
+      // const response = await axios.post(apiUrl, requestData, {
+      //   headers: {
+      //     "x-api-key": PRFCO_API_KEY,
+      //     "Content-Type": "application/json",
+      //   },
+      // });
+
+      // if (response.data.error) {
+      //   console.log("Error from API:", response.data.message);
+
+      //   return res.status(500).json({ error: response.data.message });
+      // }
+
+      let localData = {
+        name,
+        position,
+        currentDateIST,
+        URL: `i= ${i}`,
+        // URL: response.data.url || ``,
+      };
+
+      pdfUrls.push(localData);
+    }
+
+    const csv = parse(pdfUrls);
+
+    const filePath = path.join(__dirname, "./../CSV/pdf_data.csv");
+
+    fs.writeFileSync(filePath, csv, "utf8");
+
+    console.log("CSV file saved successfully:", filePath);
+
+    return res.status(202).json({
+      status: true,
+      message: "Data Generated",
+      pdfUrls,
+    });
   } catch (error) {
     console.error("Error generating PDF:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-// Asynchronous function to read data from a Google Sheet.
 async function readSheet() {
   const sheets = google.sheets({ version: "v4", auth });
   const spreadsheetId = "1sbsjAjsR9zPAybDascnUda3U4IXmGeSfZ4ytGaFfm_g";
-  const range = "Job!A1:E10"; // Specifies the range to read.
+  const range = "Job!A1:E10";
 
   try {
     const response = await sheets.spreadsheets.values.get({
@@ -85,7 +116,7 @@ async function readSheet() {
 
     return rows;
   } catch (error) {
-    console.error("error", error); // Logs errors.
+    console.error("error", error);
   }
 }
 
