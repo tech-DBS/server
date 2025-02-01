de37365eae64416d8dbd6fe43f5fc1b7c7dd2d6d
Rishav Singh <rishavrajcrj@gmail.com>
2025-02-01 23:29:46 +0530

    Update job.js

--- controllers/job.js
+++ controllers/job.js
@@ -4,8 +4,16 @@ const { validationResult } = require("express-validator");
 const fs = require("fs");
 const path = require("path");
 
+// Sheets
+const { google } = require("googleapis");
+
 // axios
 const axios = require("axios");
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: "./google.json",
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
 
 const genLetter = async (req, res, next) => {
   try {
@@ -17,6 +25,14 @@ const genLetter = async (req, res, next) => {
     if (!name || !job) {
       return res.status(400).json({ error: "Name and job are required!" });
     }
+
+    const data = await readSheet();
+
+    return res.status(202).json({
+      status: true,
+      message: "Data Generated",
+      data,
+    });
 
     // const PRFCO_API_KEY = process.env.PRFCO_API_KEY;
 
@@ -44,28 +60,48 @@ const genLetter = async (req, res, next) => {
     //   },
     // });
 
-    if (response.data.error) {
-      console.log("Error from API:", response.data.message);
-      return res.status(500).json({ error: response.data.message });
-    }
+    // if (response.data.error) {
+    //   console.log("Error from API:", response.data.message);
+    //   return res.status(500).json({ error: response.data.message });
+    // }
 
-    if (response.data.url) {
-      return res.status(202).json({
-        status: true,
-        message: "Data Generated",
-        URL: response.data.url,
-      });
-    } else {
-      return res.status(202).json({
-        status: false,
-        message: "Unable to Generate",
-        URL: response.data.url,
-      });
-    }
+    // if (response.data.url) {
+    //   return res.status(202).json({
+    //     status: true,
+    //     message: "Data Generated",
+    //     URL: response.data.url,
+    //   });
+    // } else {
+    //   return res.status(202).json({
+    //     status: false,
+    //     message: "Unable to Generate",
+    //     URL: response.data.url,
+    //   });
+    // }
   } catch (error) {
     console.error("Error generating PDF:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+// Asynchronous function to read data from a Google Sheet.
+async function readSheet() {
+  const sheets = google.sheets({ version: "v4", auth });
+  const spreadsheetId = "1sbsjAjsR9zPAybDascnUda3U4IXmGeSfZ4ytGaFfm_g";
+  const range = "Job!A1:E10"; // Specifies the range to read.
+
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    });
+
+    const rows = response.data.values;
+
+    return rows;
+  } catch (error) {
+    console.error("error", error); // Logs errors.
+  }
+}
 
 exports.genLetter = genLetter;
