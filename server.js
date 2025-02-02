9b7f63040a0bd84be18532242180e6abecaa5c5b
Rishav Singh <rishavrajcrj@gmail.com>
2025-02-03 01:03:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json());
 
 // cros
 app.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
