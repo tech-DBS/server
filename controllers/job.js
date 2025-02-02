de0b607348ade1d5f7657c8b06f1258d91e995c0
Rishav Singh <rishavrajcrj@gmail.com>
2025-02-02 16:54:50 +0530

    Update job.js

--- controllers/job.js
+++ controllers/job.js
@@ -214,7 +214,18 @@ async function convertHtmlToPdf(
       .replaceAll("{{date}}", currentDateIST);
 
     // Launch Puppeteer
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
 
     // Set the page content
