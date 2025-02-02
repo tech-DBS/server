1216f35345fc83a8c64711d763fa4484b266a057
Rishav Singh <rishavrajcrj@gmail.com>
2025-02-02 23:27:06 +0530

    Update a.js

--- controllers/a.js
+++ controllers/a.js
@@ -1,25 +1,3 @@
-// const { chromium } = require("playwright");
-// const fs = require("fs");
-
-// (async () => {
-//   const browser = await chromium.launch();
-//   const page = await browser.newPage();
-
-//   // Load local HTML file
-//   const htmlContent = fs.readFileSync("./html/index.html", "utf8");
-//   await page.setContent(htmlContent, { waitUntil: "load" });
-
-//   // Save as PDF
-//   await page.pdf({
-//     path: "./output/output.pdf",
-//     format: "A4",
-//     printBackground: true,
-//   });
-
-//   await browser.close();
-//   console.log("PDF successfully created: output.pdf");
-// })();
-
 const htmlToPdf = require("html-to-pdf");
 
 htmlToPdf.convertHTMLFile(
