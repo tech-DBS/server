21b82ec71e7d05fa9597f97e6d19b119ec53ce7f
Rishav Singh <rishavrajcrj@gmail.com>
2025-02-27 10:59:16 +0530

    Update job.js

--- controllers/job.js
+++ controllers/job.js
@@ -321,9 +321,12 @@ async function convertHtmlToPdf(
 
     console.log(name + " " + position);
 
+    const dateMonth = formatDate();
+
     htmlContent = htmlContent
       .replaceAll("{{name}}", name)
       .replaceAll("{{position}}", position)
+      .replaceAll("{{dateMonth}}", dateMonth)
       .replaceAll("{{date}}", currentDateIST);
 
     htmlContent = htmlContent.trim();
@@ -361,6 +364,29 @@ async function convertHtmlToPdf(
   } catch (error) {
     console.error("❌ Error generating PDF:", error);
   }
+}
+
+function formatDate() {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.toLocaleString("en-IN", { month: "long" });
+
+  // Function to get ordinal suffix (st, nd, rd, th)
+  const getOrdinalSuffix = (day) => {
+    if (day > 3 && day < 21) return "th"; // Covers 4-20
+    switch (day % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  };
+
+  return `${day}${getOrdinalSuffix(day)} ${month}`;
 }
 
 exports.genLetter = genLetter;
